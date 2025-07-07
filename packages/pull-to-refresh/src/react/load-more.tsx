f6470b0e604c09454de0b390875f27b0c79ade4f
zyt-cloud <568939352@qq.com>
2025-07-07 16:54:16 +0800

    feat: load more ui

--- packages/pull-to-refresh/src/react/load-more.tsx
+++ packages/pull-to-refresh/src/react/load-more.tsx
@@ -3,13 +3,12 @@ import classes from '../styles/load-more.module.css'
 export function LoadMore() {
   return (
     <div className={classes.loadMoreWrapper}>
-      {/* <div className={classes.loadMoreLoading}>
-        <div className={classes.loadMoreBall}></div>
-        <div className={classes.loadMoreBall}></div>
-        <div className={classes.loadMoreBall}></div>
-        <div className={classes.loadMoreBall}></div>
-      </div> */}
-      <div className={classes.loader}></div>
+      <div className={classes.loadMoreLoading}>
+        <div style={{ '--delay': '0s' } as any} className={classes.loadMoreBall}></div>
+        <div style={{ '--delay': '0.2s' } as any} className={classes.loadMoreBall}></div>
+        <div style={{ '--delay': '0.4s' } as any} className={classes.loadMoreBall}></div>
+      </div>
+      <div>加载中...</div>
     </div>
   )
 }
